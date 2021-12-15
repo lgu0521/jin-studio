f8e02cd65ebd8e4a4f22730c12f72d45a4f9492e
lgu0521 <lgu0521@gmail.com>
2021-12-15 17:17:33 +0900

    아이템 마진 띄우기

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -151,25 +151,26 @@ const ContentSection = styled.div`
   text-align: center;
   @media only screen and (max-width: 600px) {
     padding: 30px 0px 15px;
-    margin-bottom: 30px;
+    margin: 30px;
   }
   @media only screen and (min-width: 600px) {
-    margin-bottom: 60px 0px 45px;
+    margin: 60px 0px 45px;
     padding: 60px 0px;
   }
   @media only screen and (min-width: 768px) {
-    margin-bottom: 80px 0px 65px;
+    margin: 80px 0px 65px;
     padding: 80px 0px;
   }
   @media only screen and (min-width: 992px) {
-    margin-bottom: 100px 0px 85px;
+    margin: 100px 0px 85px;
     padding: 100px 100px;
   }
   @media only screen and (min-width: 1200px) {
-    margin-bottom: 120px 0px 105px;
+    margin: 120px 0px 105px;
     padding: 120px 200px;
   }
 `;
+
 const BackgroundColorWithPageFullWidthLayout = styled(PageFullWidthLayout)`
   background-color: ${(props) => props.theme.colors.bg_content};
 `;
